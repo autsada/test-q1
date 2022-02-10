02bcde31f6c168606d3215adb3ed278370cedf13
autsada <autsada.t@gmail.com>
2022-02-10 14:13:02 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+// Helper function: check if prime
+function isPrime(num) {
+  if (num <= 1) return false
+  if (num === 2) return true
+
+  const square = Math.sqrt(num)
+
+  for (let i = 2; i <= square; i++) {
+    if (num % i === 0) return false
+  }
+
+  return true
 }
 
-export default App;
+// Helper function: check if fibonacci
+function isFibonacci(num, count = 1, last = 0) {
+  if (num === 0) return true
+  if (count < num) return isFibonacci(num, count + last, count)
+  if (count === num) return true
+  return false
+}
+
+function App() {
+  const [rawNum, setRawNum] = useState('')
+  const [num, setNum] = useState('')
+  const [calculation, setCalculation] = useState('prime')
+  const [result, setResult] = useState('')
+
+  // Check if raw input number is negative or not integer
+  useEffect(() => {
+    if (!rawNum) {
+      setNum('')
+      return
+    }
+
+    const numValue = +rawNum
+
+    if (numValue < 0) {
+      setNum(1)
+    } else {
+      if (!Number.isInteger(numValue)) {
+        const roundedNum = Math.round(numValue)
+        setNum(roundedNum)
+      } else {
+        setNum(numValue)
+      }
+    }
+  }, [rawNum])
+
+  // When num or/and calculation type change, run the code to recalculate the result
+  useEffect(() => {
+    if (typeof num !== 'number') {
+      setResult('')
+      return
+    }
+
+    if (calculation === 'prime') {
+      if (isPrime(num)) {
+        setResult('true')
+      } else {
+        setResult('false')
+      }
+    }
+
+    if (calculation === 'fibonacci') {
+      if (isFibonacci(num)) {
+        setResult('true')
+      } else {
+        setResult('false')
+      }
+    }
+  }, [num, calculation])
+
+  function handleChangeNum(e) {
+    if (e.target.name === 'num') {
+      const value = e.target.value
+
+      if (value) {
+        setRawNum(value)
+      } else {
+        setRawNum('')
+      }
+    }
+  }
+
+  function handleChangeCalculation(e) {
+    setCalculation(e.target.value)
+  }
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <div className='col left'>
+          <input
+            type='number'
+            name='num'
+            value={num}
+            onChange={handleChangeNum}
+          />
+        </div>
+        <div className='col center'>
+          <select value={calculation} onChange={handleChangeCalculation}>
+            <option value='prime'>isPrime</option>
+            <option value='fibonacci'>isFibonacci</option>
+          </select>
+        </div>
+        <div className='col right'>{result}</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
